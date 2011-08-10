6f1ac09b890fc2b2fa8e7cecd3783e402339f55a
Luke Stevenson <lucanos@gmail.com>
2011-08-10 10:21:53 +1000

    Commented out Theme Options Page as causing error.

--- whiteboard/functions.php
+++ whiteboard/functions.php
@@ -1,40 +1,44 @@
 <?php
   // enables widgetized sidebars
-  if( function_exists('register_sidebar') ){
+  if( function_exists( 'register_sidebar' ) ){
 
     // Sidebar Widget
     // Location: the sidebar
-    register_sidebar(array('name'=>'Sidebar',
-      'before_widget' => '<div class="widget-area widget-sidebar"><ul>',
-      'after_widget' => '</ul></div>',
-      'before_title' => '<h3>',
-      'after_title' => '</h3>',
+    register_sidebar(array(
+      'name'          => 'Sidebar' ,
+      'before_widget' => '<div class="widget-area widget-sidebar"><ul>' ,
+      'after_widget'  => '</ul></div>' ,
+      'before_title'  => '<h3>' ,
+      'after_title'   => '</h3>'
     ));
     // Header Widget
     // Location: right after the navigation
-    register_sidebar(array('name'=>'Header',
-      'before_widget' => '<div class="widget-area widget-header"><ul>',
-      'after_widget' => '</ul></div>',
-      'before_title' => '<h4>',
-      'after_title' => '</h4>',
+    register_sidebar(array(
+      'name'          => 'Header' ,
+      'before_widget' => '<div class="widget-area widget-header"><ul>' ,
+      'after_widget'  => '</ul></div>' ,
+      'before_title'  => '<h4>' ,
+      'after_title'   => '</h4>'
     ));
     // Footer Widget
     // Location: at the top of the footer, above the copyright
-    register_sidebar(array('name'=>'Footer',
-      'before_widget' => '<div class="widget-area widget-footer"><ul>',
-      'after_widget' => '</ul></div>',
-      'before_title' => '<h4>',
-      'after_title' => '</h4>',
+    register_sidebar(array(
+      'name'          => 'Footer' ,
+      'before_widget' => '<div class="widget-area widget-footer"><ul>' ,
+      'after_widget'  => '</ul></div>' ,
+      'before_title'  => '<h4>' ,
+      'after_title'   => '</h4>'
     ));
     // The Alert Widget
     // Location: displayed on the top of the home page, right after the header, right before the loop, within the content area
-    register_sidebar(array('name'=>'Alert',
-      'before_widget' => '<div class="widget-area widget-alert"><ul>',
-      'after_widget' => '</ul></div>',
-      'before_title' => '<h4>',
-      'after_title' => '</h4>',
+    register_sidebar(array(
+      'name'          => 'Alert' ,
+      'before_widget' => '<div class="widget-area widget-alert"><ul>' ,
+      'after_widget'  => '</ul></div>' ,
+      'before_title'  => '<h4>' ,
+      'after_title'   => '</h4>'
     ));
-  
+
   }
 
   // post thumbnail support
@@ -43,14 +47,12 @@
   // custom menu support
   add_theme_support( 'menus' );
   if( function_exists( 'register_nav_menus' ) ){
-    register_nav_menus(
-      array(
-        'header-menu' => 'Header Menu',
-        'sidebar-menu' => 'Sidebar Menu',
-        'footer-menu' => 'Footer Menu',
+    register_nav_menus(array(
+        'header-menu'    => 'Header Menu' ,
+        'sidebar-menu'   => 'Sidebar Menu' ,
+        'footer-menu'    => 'Footer Menu' ,
         'logged-in-menu' => 'Logged In Menu'
-      )
-    );
+    ));
   }
 
   // custom background support
@@ -73,12 +75,20 @@
   // adds Post Format support
   // learn more: http://codex.wordpress.org/Post_Formats
   add_theme_support( 'post-formats' , array(
-    'aside' , 'gallery' , 'link' , 'image' , 'quote' , 'status' , 'video' , 'audio' , 'chat'
+    'aside' ,
+    'gallery' ,
+    'link' ,
+    'image' ,
+    'quote' ,
+    'status' ,
+    'video' ,
+    'audio' ,
+    'chat'
   ) );
 
   // removes detailed login error information for security
   add_filter( 'login_errors' , create_function( '$a' , "return null;" ) );
-  
+
   // Removes Trackbacks from the comment cout
   add_filter( 'get_comments_number' , 'comment_count' , 0 );
   function comment_count( $count ){
@@ -93,9 +103,8 @@
   // invite rss subscribers to comment
   function rss_comment_footer( $content ){
     if( is_feed()
-        && comments_open() ) {
+        && comments_open() )
       $content .= 'Comments are open! <a href="'.get_permalink().'">Add yours!</a>';
-    }
     return $content;
   }
 
@@ -106,11 +115,11 @@
   add_filter( 'excerpt_more' , 'custom_excerpt_more' );
 
   // no more jumping for read more link
-  function no_more_jumping( $post ) {
+  function no_more_jumping( $post ){
     return '<a href="'.get_permalink( $post->ID ).'" class="read-more">'.'Continue Reading'.'</a>';
   }
   add_filter( 'excerpt_more' , 'no_more_jumping' );
-  
+
   // category id in body and post class
   function category_id_class( $classes ){
     global $post;
@@ -120,18 +129,27 @@
   }
   add_filter( 'post_class' , 'category_id_class' );
   add_filter( 'body_class' , 'category_id_class' );
-  
-  
+
+
   add_action( 'admin_init' , 'theme_options_init' );
   add_action( 'admin_menu' , 'theme_options_add_page' );
-  
+
   // Init plugin options to white list our options
   function theme_options_init(){
     register_setting( 'tat_options' , 'tat_theme_options' , 'theme_options_validate' );
   }
-  
+
   // Load up the menu page
+  /*
   function theme_options_add_page(){
-    add_theme_page( __( 'Theme Options' , 'tat_theme' ), __( 'Theme Options', 'tat_theme' ), 'edit_theme_options', 'theme_options', 'theme_options_do_page' );
+    add_theme_page(
+      __( 'Theme Options' , 'tat_theme' ) ,
+      __( 'Theme Options' , 'tat_theme' ) ,
+      'edit_theme_options' ,
+      'theme_options' ,
+      'theme_options_do_page'
+    );
   }
+  */
+
 ?>
